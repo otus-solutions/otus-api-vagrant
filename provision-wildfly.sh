6f387c3aa1eab2eecc3a9e80f06b36ba493e835a
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-12-22 11:14:22 -0200

    Update provision-wildfly.sh

--- provision-wildfly.sh
+++ provision-wildfly.sh
@@ -23,15 +23,15 @@ then
 
 	sed -i 's/-c $JBOSS_CONFIG $JBOSS_OPTS/-c $JBOSS_CONFIG -b 0.0.0.0 -bmanagement 0.0.0.0 $JBOSS_OPTS/g' /etc/init.d/wildfly
 
-	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Origin\"\/>/' /wildfly/standalone/configuration/standalone.xml
-	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Methods\"\/>/' /wildfly/standalone/configuration/standalone.xml
-	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Headers\"\/>/' /wildfly/standalone/configuration/standalone.xml
-	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Credentials\"\/>/' /wildfly/standalone/configuration/standalone.xml
+	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Origin\"\/>/' wildfly/standalone/configuration/standalone.xml
+	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Methods\"\/>/' wildfly/standalone/configuration/standalone.xml
+	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Headers\"\/>/' wildfly/standalone/configuration/standalone.xml
+	sed -i '/<host name="default-host" alias="localhost">/s/$/\n\t<filter-ref name=\"Access-Control-Allow-Credentials\"\/>/' wildfly/standalone/configuration/standalone.xml
 
-	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Origin\" header-name=\"Access-Control-Allow-Origin\" header-value=\"\*\"\/>/' /wildfly/standalone/configuration/standalone.xml
-	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Methods\" header-name=\"Access-Control-Allow-Methods\" header-value=\"\GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH\"\/>/' /wildfly/standalone/configuration/standalone.xml
-	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Headers\" header-name=\"Access-Control-Allow-Headers\" header-value=\"Content-Type, Authorization\"\/>/' /wildfly/standalone/configuration/standalone.xml
-	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Credentials\" header-name=\"Access-Control-Allow-Credentials\" header-value=\"true\"\/>/' /wildfly/standalone/configuration/standalone.xml
+	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Origin\" header-name=\"Access-Control-Allow-Origin\" header-value=\"\*\"\/>/' wildfly/standalone/configuration/standalone.xml
+	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Methods\" header-name=\"Access-Control-Allow-Methods\" header-value=\"\GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH\"\/>/' wildfly/standalone/configuration/standalone.xml
+	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Headers\" header-name=\"Access-Control-Allow-Headers\" header-value=\"Content-Type, Authorization\"\/>/' wildfly/standalone/configuration/standalone.xml
+	sed -i '/<filters>/s/$/\n\t<response-header name=\"Access-Control-Allow-Credentials\" header-name=\"Access-Control-Allow-Credentials\" header-value=\"true\"\/>/' wildfly/standalone/configuration/standalone.xml
 
 	sudo ./wildfly/bin/add-user.sh admin admin
 
